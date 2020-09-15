6662288fdedf6c14346fe510c8397ab947a379ee
Pany <panypham39@gmail.com>
2020-09-15 10:20:34 +0700

    Update Biometric

--- app/src/main/java/com/raywenderlich/android/securepass/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/securepass/MainActivity.kt
@@ -35,17 +35,100 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    setTheme(R.style.AppTheme)
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-  // TODO: define authUser
+        val executor = ContextCompat.getMainExecutor(this)
+        val biometricManager = BiometricManager.from(this)
+
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                authUser(executor)
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Toast.makeText(
+                    this,
+                    getString(R.string.error_msg_no_biometric_hardware),
+                    Toast.LENGTH_LONG
+                ).show()
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Toast.makeText(
+                    this,
+                    getString(R.string.error_msg_biometric_hw_unavailable),
+                    Toast.LENGTH_LONG
+                ).show()
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                Toast.makeText(
+                    this,
+                    getString(R.string.error_msg_biometric_not_setup),
+                    Toast.LENGTH_LONG
+                ).show()
+        }
+
+        val sharedPreferences: SharedPreferences =
+            getSharedPreferences("user_data", Context.MODE_PRIVATE)
+        btnSave.setOnClickListener {
+            val editor = sharedPreferences.edit()
+            editor.putString("Name", etName.text.toString())
+            editor.putString("Email", etEmail.text.toString())
+            editor.putString("Phone", etPhone.text.toString())
+
+            editor.apply()
+
+            etName.setText("")
+            etEmail.setText("")
+            etPhone.setText("")
+        }
+
+        etName.setText(sharedPreferences.getString("Name", ""))
+        etEmail.setText(sharedPreferences.getString("Email", ""))
+        etPhone.setText(sharedPreferences.getString("Phone", ""))
+    }
+
+    // TODO: define authUser
+    private fun authUser(executor: Executor) {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle(getString(R.string.auth_title))
+            .setSubtitle(getString(R.string.auth_subtitle))
+            .setDescription(getString(R.string.auth_description))
+            .setDeviceCredentialAllowed(true)
+            .build()
+
+        val biometricPrompt =
+            BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    main_layout.visibility = View.VISIBLE
+                }
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.error_msg_auth_error, errString),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.error_msg_auth_failed),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+
+        biometricPrompt.authenticate(promptInfo)
+    }
 }
